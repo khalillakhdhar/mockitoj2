65c404e7c20e612370d79179910a5f992c701d5f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-26 12:57:42 +0100

    instanciation

--- src/main/java/mockitoj2/demo/services/RecordService.java
+++ src/main/java/mockitoj2/demo/services/RecordService.java
@@ -1,5 +1,25 @@
 package mockitoj2.demo.services;
 
+import mockitoj2.demo.dao.DatabaseDAO;
+import mockitoj2.demo.dao.NetworkDAO;
+
 public class RecordService {
+DatabaseDAO database;
+NetworkDAO network;
+public DatabaseDAO getDatabase() {
+	return database;
+}
+public void setDatabase(DatabaseDAO database) {
+	this.database = database;
+}
+public NetworkDAO getNetwork() {
+	return network;
+}
+public void setNetwork(NetworkDAO network) {
+	this.network = network;
+}
+
+
+
 
 }
