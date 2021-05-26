8b4c358ac72e1f7b900c5a9e24c54989355928ec
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-26 13:09:42 +0100

    init mock

--- src/main/java/mockitoj2/demo/ApplicationTest.java
+++ src/main/java/mockitoj2/demo/ApplicationTest.java
@@ -1,5 +1,18 @@
 package mockitoj2.demo;
 
+import org.junit.Before;
+import org.mockito.MockitoAnnotations;
+
 public class ApplicationTest {
 
+	
+	@Before
+	public void initMocks()
+	{
+		MockitoAnnotations.initMocks(this);
+		
+		
+	}
+	
+	
 }
