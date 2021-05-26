b40836a9a4eff094a36f4b9faaa7d623d76de806
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-26 13:50:20 +0100

    data

--- src/main/java/mockitoj2/demo/ApplicationTest.java
+++ src/main/java/mockitoj2/demo/ApplicationTest.java
@@ -1,7 +1,16 @@
 package mockitoj2.demo;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.internal.verification.Times;
+import mockitoj2.demo.dao.DatabaseDAO;
+import mockitoj2.demo.dao.NetworkDAO;
+import mockitoj2.demo.services.RecordService;
 
 public class ApplicationTest {
 
@@ -13,6 +22,21 @@ public class ApplicationTest {
 		
 		
 	}
-	
+	@InjectMocks
+	RecordService recordService;
+	@Mock
+	DatabaseDAO databaseMock;
+	@Mock
+	NetworkDAO networkMock;
+	@Test
+	public void saveTest()
+	{
+		boolean saved= recordService.save("tmp.txt");
+		assertEquals(true,saved);
+	//	verify(databaseMock , times(1)).save("temp.txt");
+		
+		
+		
+	}
 	
 }
